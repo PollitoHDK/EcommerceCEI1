495e44fa2ca0589d8193c5c482d97f2fd6a8280a
SaraLuciaaa <saludipu71@gmail.com>
2024-11-17 01:30:47 -0500

    feat(navbar): add conditional rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/client/home';
 import Register from './pages/auth';
@@ -8,15 +8,25 @@ import { ThemeProvider } from '@material-tailwind/react';
 import Navbar from './components/navbar';
 
 const App = () => {
-  const location = useLocation();
+  const location = useLocation(); // Obtén la ubicación actual
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem('user'); // Recupera usuario guardado
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  // Función para cerrar sesión
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('user'); // Limpia el almacenamiento
+  };
 
   return (
     <ThemeProvider>
-      {/* Renderiza la Navbar solo si la ruta actual no es /auth */}
-      {location.pathname !== '/auth' && <Navbar />}
+      <Navbar user={user} logout={logout} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Register />} />
+        <Route path="/signin" element={<Register setUser={setUser} />} />
+        <Route path="/signup" element={<Register setUser={setUser} />} />
         <Route path="/stock" element={<StockManagement />} />
       </Routes>
     </ThemeProvider>
